d29763baf350ae991c7c9969c7b4fd009cb88474
birajshrestha72 <birajshrestha72@gmail.com>
2025-07-31 11:13:45 +0545

    CII

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,4 +1,3 @@
-
 # Initialize Flask app
 from flask import Flask
 from flask_cors import CORS
@@ -8,7 +7,7 @@ app = Flask(__name__)
 CORS(app)
 
 # MongoDB connection (update URI as needed)
-client = MongoClient('mongodb://localhost:27017/')
+client = MongoClient('mongodb+srv://<username>:<password>@<cluster-url>/<dbname>?retryWrites=true&w=majority')
 db = client['discount_db']
 
 from app import routes
